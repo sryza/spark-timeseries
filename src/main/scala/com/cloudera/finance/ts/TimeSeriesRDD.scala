58f6d0cf423f3a60a9e6a67428a4009542f08cc1
Sandy Ryza <sandy@cloudera.com>
2015-04-07 11:01:45 -0700

    Implement toSamples

--- src/main/scala/com/cloudera/finance/ts/TimeSeriesRDD.scala
+++ src/main/scala/com/cloudera/finance/ts/TimeSeriesRDD.scala
@@ -15,6 +15,8 @@
 
 package com.cloudera.finance.ts
 
+import scala.collection.mutable.ArrayBuffer
+
 import breeze.linalg._
 
 import org.apache.spark.SparkContext._
@@ -22,7 +24,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.util.StatCounter
 
 import org.joda.time.DateTime
-import org.apache.spark.{TaskContext, Partition}
+import org.apache.spark.{Partition, Partitioner, TaskContext}
 
 class TimeSeriesRDD[K](val index: DateTimeIndex, parent: RDD[(K, Vector[Double])])
   extends RDD[(K, Vector[Double])](parent) {
@@ -62,9 +64,97 @@ class TimeSeriesRDD[K](val index: DateTimeIndex, parent: RDD[(K, Vector[Double])
     throw new UnsupportedOperationException()
   }
 
-  def toSamples(numPartitions: Int): RDD[(DateTime, Vector[Double])] = {
+  /**
+   * Essentially transposes the time series matrix to create an RDD where each record contains a
+   * single instant in time and all the values that correspond to it. Involves a shuffle operation.
+   *
+   * In the returned RDD, the ordering of values within each record corresponds to the ordering of
+   * the time series records in the original RDD. The records are ordered by time.
+   */
+  def toSamples(nPartitions: Int): RDD[(DateTime, Vector[Double])] = {
+    val maxChunkSize = 20
 
-    throw new UnsupportedOperationException()
+    val dividedOnMapSide = mapPartitionsWithIndex { case (partitionId, iter) =>
+      new Iterator[((Int, Int), Vector[Double])] {
+        // Each chunk is a buffer of time series
+        var chunk: ArrayBuffer[Vector[Double]] = _
+        // Current date time.  Gets reset for every chunk.
+        var dtLoc: Int = _
+        var chunkId: Int = -1
+
+        def hasNext: Boolean = iter.hasNext || dtLoc < index.size
+        def next(): ((Int, Int), Vector[Double]) = {
+          if (chunk == null || dtLoc == index.size) {
+            chunk.clear()
+            while (chunk.size < maxChunkSize && iter.hasNext) {
+              chunk += iter.next._2
+            }
+            dtLoc = 0
+            chunkId += 1
+          }
+
+          val arr = new Array[Double](chunk.size)
+          var i = 0
+          while (i < chunk.size) {
+            arr(i) = chunk(i)(dtLoc)
+            i += 1
+          }
+          ((dtLoc, partitionId * maxChunkSize + chunkId), new DenseVector(arr))
+        }
+      }
+    }
+
+    // At this point, dividedOnMapSide is an RDD of snippets of full samples that will be
+    // assembled on the reduce side.  Each key is a tuple of
+    // (date-time, position of snippet in full sample)
+
+    val partitioner = new Partitioner() {
+      def numPartitions: Int = nPartitions
+      def getPartition(key: Any): Int = key.asInstanceOf[(Int, _, _)]._1 / nPartitions
+    }
+    implicit val ordering = new Ordering[(Int, Int)] {
+      override def compare(a: (Int, Int), b: (Int, Int)): Int = {
+        val dtDiff = a._1 - b._1
+        if (dtDiff != 0){
+          dtDiff
+        } else {
+          a._2 - b._2
+        }
+      }
+    }
+    val repartitioned = dividedOnMapSide.repartitionAndSortWithinPartitions(partitioner)
+    repartitioned.mapPartitions { iter: Iterator[((Int, Int), Vector[Double])] =>
+      new Iterator[(DateTime, Vector[Double])] {
+        var cur = if (iter.hasNext) iter.next() else null
+        def hasNext: Boolean = next != null
+
+        def next(): (DateTime, Vector[Double]) = {
+          val snippets = new ArrayBuffer[Vector[Double]]()
+          var vecSize = 0
+          val dtLoc = cur._1._1
+          while (cur != null && cur._1._1 == dtLoc) {
+            snippets += cur._2
+            vecSize += cur._2.size
+            if (iter.hasNext) {
+              cur = iter.next()
+            }
+          }
+          val dt = index.dateTimeAtLoc(dtLoc)
+          val vec = if (snippets.size == 1) {
+            snippets(0)
+          } else {
+            val resVec = DenseVector.zeros[Double](vecSize)
+            var i = 0
+            snippets.foreach { snip =>
+              resVec(i until snip.length) := snip
+              i += snip.length
+            }
+            resVec
+          }
+          (dt, vec)
+        }
+      }
+    }
   }
 
   def compute(split: Partition, context: TaskContext): Iterator[(K, Vector[Double])] = {
@@ -83,8 +173,4 @@ object TimeSeriesRDD {
     }
     new TimeSeriesRDD(targetIndex, rdd)
   }
-}
-
-class TimeSamples {
-
 }
