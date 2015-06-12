6c2e2a6645ac01cff7fbcce57b1a4645ba83b8df
jose.cambronero <jose.cambronero@cloudera.com>
2015-06-12 11:40:01 -0700

    reworked bgtest to take residuals, added bptest

--- src/test/scala/com/cloudera/sparkts/TimeSeriesStatisticalTestsSuite.scala
+++ src/test/scala/com/cloudera/sparkts/TimeSeriesStatisticalTestsSuite.scala
@@ -3,6 +3,7 @@
  *
  * Cloudera, Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"). You may not use this file except in
+ * Version 2.0 (the "License"). You may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,6 +15,8 @@
  */
 
 package com.cloudera.sparkts
+
+import org.apache.commons.math.stat.regression.OLSMultipleLinearRegression
 
 import scala.Double.NaN
 
@@ -31,18 +34,62 @@ class TimeSeriesStatisticalTestsSuite extends FunSuite with ShouldMatchers {
     val rand = new MersenneTwister(5L) 
     val n = 100
     val coef = 0.5 // coefficient for lagged series
+
     val x = Array.fill(n / 2)(Array(1.0, -1.0)).flatten
-    val y1 = x.map(_ + 1 + rand.nextDouble) // stationary 
-    // AR(1), recursive filter with 0.5 coef
-    val y2 = y1.scanLeft(0.0) { case (prior, curr) => prior * coef + curr }.tail 
+    // stationary series
+    val y1 = x.map(_ + 1 + rand.nextGaussian())
+    // AR(1) series, recursive filter with coef coefficient
+    val y2 = y1.scanLeft(0.0) { case (prior, curr) => prior * coef + curr }.tail
+
     val pthreshold = 0.05
-    
+
+    val OLS1 = new OLSMultipleLinearRegression()
+    OLS1.newSampleData(y1, x.map(Array(_)))
+    val resids1 = OLS1.estimateResiduals()
+
+    val OLS2 = new OLSMultipleLinearRegression()
+    OLS2.newSampleData(y2, x.map(Array(_)))
+    val resids2 = OLS2.estimateResiduals()
+
     // there should be no evidence of serial correlation
-    bgtest(new DenseVector(y1), new DenseMatrix(x.length, 1, x), 1)._2 should be > pthreshold 
-    bgtest(new DenseVector(y1), new DenseMatrix(x.length, 1, x), 4)._2 should be > pthreshold 
+    bgtest(new DenseVector(resids1), new DenseMatrix(x.length, 1, x), 1)._2 should be > pthreshold
+    bgtest(new DenseVector(resids1), new DenseMatrix(x.length, 1, x), 4)._2 should be > pthreshold
     // there should be evidence of serial correlation
-    bgtest(new DenseVector(y2), new DenseMatrix(x.length, 1, x), 1)._2 should be < pthreshold
-    bgtest(new DenseVector(y2), new DenseMatrix(x.length, 1, x), 4)._2 should be < pthreshold
+    bgtest(new DenseVector(resids2), new DenseMatrix(x.length, 1, x), 1)._2 should be < pthreshold
+    bgtest(new DenseVector(resids2), new DenseMatrix(x.length, 1, x), 4)._2 should be < pthreshold
   }
+  
+  test("breusch-pagan") {
+    // Replicating the example provided by R package lmtest for bptest
+    val rand = new MersenneTwister(5L)
+    val n = 100
+    val x = Array.fill(n / 2)(Array(-1.0, 1.0)).flatten
+    
+    // homoscedastic residuals with variance 1 throughout
+    val err1 = Array.fill(n)(rand.nextGaussian)
+    // heteroskedastic residuals with alternating variance of 1 and 4
+    val varFactor = 2
+    val err2 = err1.zipWithIndex.map { case (x, i) =>  if(i % 2 == 0) x * varFactor else x }
+    
+    // generate dependent variables
+    val y1 = x.zip(err1).map { case (xi, ei) => xi + ei + 1 }
+    val y2 = x.zip(err2).map { case (xi, ei) => xi + ei + 1 }
+    
+    // create models and calculate residuals
+    val OLS1 = new OLSMultipleLinearRegression()
+    OLS1.newSampleData(y1, x.map(Array(_)))
+    val resids1 = OLS1.estimateResiduals()
+
+    val OLS2 = new OLSMultipleLinearRegression()
+    OLS2.newSampleData(y2, x.map(Array(_)))
+    val resids2 = OLS2.estimateResiduals()
+    
+    val pthreshold = 0.05
+    // there should be no evidence of heteroskedasticity
+    bptest(new DenseVector(resids1), new DenseMatrix(x.length, 1, x))._2 should be > pthreshold
+    // there should be evidence of heteroskedasticity
+    bptest(new DenseVector(resids2), new DenseMatrix(x.length, 1, x))._2 should be < pthreshold
+	
+	}
 
 }
