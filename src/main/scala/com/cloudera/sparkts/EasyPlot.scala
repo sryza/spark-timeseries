89a642b090b88b10544e617541b25a6aafe3538a
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2015-06-28 15:24:52 -0700

    added pacf, refactored acf

--- src/main/scala/com/cloudera/sparkts/EasyPlot.scala
+++ src/main/scala/com/cloudera/sparkts/EasyPlot.scala
@@ -33,7 +33,7 @@ object EasyPlot {
   def ezplot(arr: Array[Double], style: Char): Unit = {
     val f = Figure()
     val p = f.subplot(0)
-    p += plot((0 until arr.length).map(_.toDouble).toArray, arr, style = style)
+    p += plot(arr.indices.map(_.toDouble).toArray, arr, style = style)
   }
 
   def ezplot(arr: Array[Double]): Unit = ezplot(arr, '-')
@@ -50,35 +50,54 @@ object EasyPlot {
   def ezplot(vecs: Seq[Vector[Double]]): Unit = ezplot(vecs, '-')
 
   def acfPlot(data: Array[Double], maxLag: Int, conf: Double = 0.95): Unit = {
+    // calculate correlations and confidence bound
     val autoCorrs = UnivariateTimeSeries.autocorr(data, maxLag)
-    val nCorrs = autoCorrs.length
-    val nSample = data.length
-    val stdNormDist = new NormalDistribution(0, 1)
+    val confVal = calcConfVal(conf, data.length)
 
-    val pVal = (1 - conf) / 2.0
-    val confVal = stdNormDist.inverseCumulativeProbability(1 - pVal) / Math.sqrt(nSample)
+    // Basic plot information
     val f = Figure()
     val p = f.subplot(0)
-
-    // Basic information
-    p.title = "Auto correlation function"
+    p.title = "Autocorrelation function"
     p.xlabel = "Lag"
-    p.ylabel = "Correlation"
+    p.ylabel = "Autocorrelation"
+    drawCorrPlot(autoCorrs, confVal, p)
+  }
 
+  def pacfPlot(data: Array[Double], maxLag: Int, conf: Double = 0.95): Unit = {
+    // create AR(maxLag) model, retrieve coefficients and calculate confidence bound
+    val model = Autoregression.fitModel(new DenseVector(data), maxLag)
+    val pCorrs = model.coefficients // partial autocorrelations are the coefficients in AR(n) model
+    val confVal = calcConfVal(conf, data.length)
+
+    // Basic plot information
+    val f = Figure()
+    val p = f.subplot(0)
+    p.title = "Partial autocorrelation function"
+    p.xlabel = "Lag"
+    p.ylabel = "Partial Autocorrelation"
+    drawCorrPlot(pCorrs, confVal, p)
+  }
+
+  private[sparkts] def calcConfVal(conf:Double, n: Int) = {
+    val stdNormDist = new NormalDistribution(0, 1)
+    val pVal = (1 - conf) / 2.0
+    stdNormDist.inverseCumulativeProbability(1 - pVal) / Math.sqrt(n)
+  }
+
+  private[sparkts] def drawCorrPlot(corrs: Array[Double], confVal: Double, p: Plot) = {
     // make decimal ticks visible
     p.setYAxisDecimalTickUnits()
-
     // plot correlations as vertical lines
-    val verticalLines = autoCorrs.zipWithIndex.map { case (corr, ix) =>
+    val verticalLines = corrs.zipWithIndex.map { case (corr, ix) =>
       (Array(ix.toDouble + 1, ix.toDouble + 1), Array(0, corr))
-      }
+    }
     verticalLines.foreach { case (xs, ys) => p += plot(xs, ys) }
-
     // plot confidence intervals as horizontal lines
+    val n = corrs.length
     Array(confVal, -1 * confVal).foreach { conf =>
-      val xs = (0 to nCorrs).toArray.map(_.toDouble)
-      val ys = Array.fill(nCorrs + 1)(conf)
-       p += plot(xs, ys, '-', colorcode = "red")
-      }
+      val xs = (0 to n).toArray.map(_.toDouble)
+      val ys = Array.fill(n + 1)(conf)
+      p += plot(xs, ys, '-', colorcode = "red")
+    }
   }
 }
