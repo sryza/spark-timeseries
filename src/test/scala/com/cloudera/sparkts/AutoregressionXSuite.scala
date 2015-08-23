959bbcf94fd790a00660f68abf6f704030c6972f
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2015-08-22 20:25:22 -0700

    cleaned up tests

--- src/test/scala/com/cloudera/sparkts/AutoregressionXSuite.scala
+++ src/test/scala/com/cloudera/sparkts/AutoregressionXSuite.scala
@@ -29,39 +29,37 @@ class AutoregressionXSuite extends FunSuite {
   val X = Array.fill(nRows, nCols)(rand.nextGaussian())
   val intercept = rand.nextGaussian * 10
 
-  // Tests an autoregressive model where the exogenous variables are not lagged
+  // tests an autoregressive model where the exogenous variables are not lagged
   test("fit ARX(1, 0, true)") {
     val xCoeffs = Array(0.8, 0.2)
     val rawY = X.map(_.zip(xCoeffs).map { case (b, v) => b * v }.sum + intercept)
-    val arYCoeff = 0.4
-    val y = rawY.scanLeft(0.0) { case (priorY, currY) => currY + priorY * arYCoeff }.tail
+    val arCoeff = 0.4
+    val y = rawY.scanLeft(0.0) { case (priorY, currY) => currY + priorY * arCoeff }.tail
     val dy = new DenseVector(y)
     val dx = new DenseMatrix(rows = X.length, cols = X.head.length, data = X.transpose.flatten)
     val model = AutoregressionX.fitModel(dy, dx, 1, 0, includeOriginalX = true)
-    val Array(modelAR, modelX1, modelX2) = model.coefficients
-    val modelC = model.c
+    val combinedCoeffs = Array(arCoeff) ++ xCoeffs
 
-    modelC should be (intercept +- 1e-4)
-    modelAR should be (arYCoeff +- 1e-4)
-    modelX1 should be (xCoeffs(0) +- 1e-4)
-    modelX2 should be (xCoeffs(1) +- 1e-4)
+    model.c should be (intercept +- 1e-4)
+    for (i <- combinedCoeffs.indices) {
+      model.coefficients(i) should be (combinedCoeffs(i) +- 1e-4)
+    }
   }
 
   // tests a model with no autoregressive term but with lagged exogenous variables
   test("fit ARX(0, 1, false) model") {
     val xCoeffs = Array(0.4, 0.15)
-    val xLagged = Lag.lagMatTrimBoth(X, 1, false)
+    val xLagged = Lag.lagMatTrimBoth(X, 1)
     val y = xLagged.map(_.zip(xCoeffs).map { case (b, v) => b * v }.sum + intercept)
     val dy = new DenseVector(Array(0.0) ++ y)
     // note that we provide the original X matrix to the fitting functiond
     val dx = new DenseMatrix(rows = X.length, cols = X.head.length, data = X.transpose.flatten)
     val model = AutoregressionX.fitModel(dy, dx, 0, 1, includeOriginalX = false)
-    val Array(modelX1, modelX2) = model.coefficients
-    val modelC = model.c
 
-    modelC should be (intercept +- 1e-4)
-    modelX1 should be (xCoeffs(0) +- 1e-4)
-    modelX2 should be (xCoeffs(1) +- 1e-4)
+    model.c should be (intercept +- 1e-4)
+    for (i <- xCoeffs.indices) {
+      model.coefficients(i) should be (xCoeffs(i) +- 1e-4)
+    }
   }
 
   // this test simply reduces to a normal regression model
@@ -71,50 +69,49 @@ class AutoregressionXSuite extends FunSuite {
     val y = X.map(_.zip(xCoeffs).map { case (b, v) => b * v }.sum + intercept)
     val dy = new DenseVector(y)
     val dx = new DenseMatrix(rows = X.length, cols = X.head.length, data = X.transpose.flatten)
-    val model = AutoregressionX.fitModel(dy, dx, 1, 0, includeOriginalX = true)
-    val Array(modelAR, modelX1, modelX2) = model.coefficients
-    val modelC = model.c
+    val model = AutoregressionX.fitModel(dy, dx, 0, 0, includeOriginalX = true)
 
-    modelC should be (intercept +- 1e-4)
-    modelX1 should be (xCoeffs(0) +- 1e-4)
-    modelX2 should be (xCoeffs(1) +- 1e-4)
+    model.c should be (intercept +- 1e-4)
+    for (i <- xCoeffs.indices) {
+      model.coefficients(i) should be (xCoeffs(i) +- 1e-4)
+    }
   }
 
+  // tests a model with no autoregressive term but with lagged exogenous variables
+  // of order 2 and inclusive of the original X values
   test("fit ARX(0, 2, true) model") {
     val xLagCoeffs = Array(0.4, 0.15, 0.2, 0.7)
-    val xLagged = Lag.lagMatTrimBoth(X, 2, false)
+    val xLagged = Lag.lagMatTrimBoth(X, 2)
     val yLaggedPart = xLagged.map(_.zip(xLagCoeffs).map { case (b, v) => b * v }.sum )
     val xNormalCoeffs = Array(0.3, 0.5)
     val yNormalPart = X.map(_.zip(xNormalCoeffs).map { case (b, v) => b * v }.sum )
-    val xCoeffs = xLagCoeffs ++ xNormalCoeffs
     val y = yLaggedPart.zip(yNormalPart.drop(2)).map { case (l, n) => l + n + intercept }
 
     val dy = new DenseVector(Array(0.0, 0.0) ++ y)
     val dx = new DenseMatrix(rows = X.length, cols = X.head.length, data = X.transpose.flatten)
     val model = AutoregressionX.fitModel(dy, dx, 0, 2, includeOriginalX = true)
-    val modelC = model.c
+    val combinedCoeffs = xLagCoeffs ++ xNormalCoeffs
 
-    modelC should be (intercept +- 1e-4)
-    for (i <- 0 until xCoeffs.length) {
-      model.coefficients(i) should be(xCoeffs(i) +- 1e-4)
+    model.c should be (intercept +- 1e-4)
+    for (i <- combinedCoeffs.indices) {
+      model.coefficients(i) should be (combinedCoeffs(i) +- 1e-4)
     }
   }
 
   test("fit ARX(1, 1, false) model") {
     val xCoeffs = Array(0.8, 0.2)
-    val xLagged = Lag.lagMatTrimBoth(X, 1, false)
+    val xLagged = Lag.lagMatTrimBoth(X, 1)
     val rawY = xLagged.map(_.zip(xCoeffs).map { case (b, v) => b * v }.sum + intercept)
-    val arYCoeff = 0.4
-    val y = rawY.scanLeft(0.0) { case (priorY, y) => y + priorY * arYCoeff }.tail
+    val arCoeff = 0.4
+    val y = rawY.scanLeft(0.0) { case (priorY, currY) => currY + priorY * arCoeff }.tail
     val dy = new DenseVector(Array(0.0) ++ y)
     val dx = new DenseMatrix(rows = X.length, cols = X.head.length, data = X.transpose.flatten)
     val model = AutoregressionX.fitModel(dy, dx, 1, 1, includeOriginalX = false)
-    val Array(modelAR, modelX1, modelX2) = model.coefficients
-    val modelC = model.c
+    val combinedCoeffs = Array(arCoeff) ++ xCoeffs
 
-    modelC should be (intercept +- 1e-4)
-    modelAR should be (arYCoeff +- 1e-4)
-    modelX1 should be (xCoeffs(0) +- 1e-4)
-    modelX2 should be (xCoeffs(1) +- 1e-4)
+    model.c should be (intercept +- 1e-4)
+    for (i <- combinedCoeffs.indices) {
+      model.coefficients(i) should be (combinedCoeffs(i) +- 1e-4)
+    }
   }
 }
