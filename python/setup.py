c044bdc964600627b17b160645b2cba7aaf76dff
Sandy Ryza <sandyryza@gmail.com>
2015-10-28 13:46:28 -0700

    setup.py enhancements

--- python/setup.py
+++ python/setup.py
@@ -2,9 +2,26 @@ from setuptools import setup, find_packages
 import os
 
 VERSION = '0.1.0'
+JAR_FILE = 'sparkts-' + VERSION + '-jar-with-dependencies.jar'
 
 setup(
-    name='sparktimeseries',
+    name='sparkts',
+    description = 'A library for analyzing large time series data with Apache Spark',
+    author = 'Sandy Ryza',
+    author_email = 'sandy@cloudera.com',
+    url = 'https://github.com/sryza/spark-timeseries',
     version=VERSION,
-    packages=find_packages()
+    packages=find_packages(),
+    classifiers = [],
+    package_data = {
+        'sparkts.jar': ['../target/' + JAR_FILE]
+    },
+    install_requires = [
+        'pandas >= 0.13',
+        'numpy >= 1.9.2'
+    ],
+    test_requires = [
+        'nose == 1.3.7',
+        'unittest2 >= 1.0.0'
+    ]
 )
