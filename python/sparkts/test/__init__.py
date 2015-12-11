a249668a749788f74ed4b6bfdd44fdc25c5e1897
Gábor Lipták <gliptak@gmail.com>
2015-12-10 22:18:03 -0500

    Modify imports and except for Python3

--- python/sparkts/test/__init__.py
+++ python/sparkts/test/__init__.py
@@ -9,7 +9,7 @@ def run_cmd(cmd):
     try:
         out = sub.check_output(cmd, shell=True, stderr=sub.STDOUT)
         return out
-    except sub.CalledProcessError, err:
+    except sub.CalledProcessError as err:
         logging.error("The failed test setup command was [%s]." % err.cmd)
         logging.error("The output of the command was [%s]" % err.output)
         raise
